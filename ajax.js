01ae53d8ffe8be1ff452f296b9766467f682fb92
sarmad <30446215+petarian80@users.noreply.github.com>
2017-08-01 01:15:50 +0500

    Add files via upload

--- ajax.js
+++ ajax.js
@@ -41,34 +41,27 @@ function checkDatabase(){
 }
 
 
+/*$(document).ready(function() {
 
-$(document).ready(function(){
-$("#login").click(function(){
-var username = $("#username").val();
-var password = $("#password").val();
-// Checking for blank fields.
-if( username =='' || password ==''){
-$('input[type="text"],input[type="password"]').css("border","2px solid red");
-$('input[type="text"],input[type="password"]').css("box-shadow","0 0 3px red");
-alert("Please fill all fields...!!!!!!");
-}else {
-$.post("login.php",{ username: username, password:password},
-function(data) {
-if(data=='Invalid username or password.......') {
-$('input[type="text"]').css({"border":"2px solid red","box-shadow":"0 0 3px red"});
-$('input[type="password"]').css({"border":"2px solid #00F5FF","box-shadow":"0 0 5px #00F5FF"});
-alert(data);
-}else if(data=='username or Password is wrong...!!!!'){
-$('input[type="text"],input[type="password"]').css({"border":"2px solid red","box-shadow":"0 0 3px red"});
-alert(data);
-} else if(data=='Successfully Logged in...'){
-$("form")[0].reset();
-$('input[type="text"],input[type="password"]').css({"border":"2px solid #00F5FF","box-shadow":"0 0 5px #00F5FF"});
-alert(data);
-} else{
-alert(data);
-}
-});
-}
-});
-})
+    $('#loginform').click(function() {
+
+        $.ajax({
+            type: "POST",
+            url: 'admin/login.php',
+            data: {
+                username: $("#username").val(),
+                password: $("#password").val()
+            },
+            success: function(data)
+            {
+                if (data === 'Correct') {
+                    window.location.replace('login/login.php');
+                }
+                else {
+                    alert(data);
+                }
+            }
+        });
+
+    });
+*/
--- login.php
+++ login.php
@@ -2,28 +2,71 @@
 
  		include('database.php');
 		
-		$db = new database();
-		$conn = $db->get_database_connection();
+		//$db = new database();
+	//	$conn = $db->get_database_connection();
 		
+			$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "inventory";
+
+		// Create connection
+		$conn = new mysqli($servername, $username, $password, $dbname);
 		
-		//$username = mysql_real_escape_string ($_POST['username']);
-		//$password = mysql_real_escape_string (md5($_POST['password']));
+		switch($_REQUEST['decision']) {
+
+    case 'submit': 
+               
+			   $sql = "INSERT INTO recieved (part_no, item_name, a_u, quantity, rate, po, crv,date_time,unit ) values ('$_POST[part_no]','$_POST[item_name]','$_POST[a_u]','$_POST[quantity]','$_POST[rate]','$_POST[po]','$_POST[crv]','$_POST[date_time]','$_POST[unit]')";
+
+if (!mysqli_query($conn,$sql))
+{die('Error: ' . mysql_error());
+
+}
+else
+echo "1 record added";
+			    break;
+
+    case 'view': 
+			$sql = " select * from recieved where date_time = (select max(date_time) from recieved)";
 		
-		$sql = "SELECT  user_name, password  FROM login WHERE user_name='admin2' AND password = 'admin'";
+				if (!mysqli_query($conn,$sql)){
+					die('Error: ' . mysqli_error());
+					}
+					else{
+						$result = mysqli_query($conn,$sql);
+						while($row = mysqli_fetch_array($result, MYSQL_ASSOC)) {
+		
+						  print_r($row);
+								}
+					
+								}
+						break;
+
+    case 'delete': 
 	
+	$sql = " select * from recieved where date_time = (select max(date_time) from recieved)";
+	$sql1 = " select * from recieved";
+	if (! mysqli_query($conn,$sql)){
+	die('Error: ' . mysql_error());
+	//echo "error";
+	}
+		else{
+				$result = mysqli_query($conn,$sql);
+				echo "row deleted";
+				$result1 = mysqli_query($conn,$sql1);
+				while($row = mysqli_fetch_array($result1, MYSQL_ASSOC)) {
 
-	$result = mysqli_query($conn,$sql);
+     print_r($row);
+}
+}
+                        break;
 
-	$rows = mysqli_fetch_array($result);
+    case 'update':
+                    break;
+}
+
+
 		
-		if(mysqli_num_rows($result)>0 ){
-			
-			print_r($rows);
-				
-		}
-		//if(strcmp($row['password'],$password)==0)
-		
- //echo "login successful";
- //else
- //echo "login failed";
- ?>
+
+?>
